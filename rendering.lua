e86a739fa145efa0a82d8e373f634da61fae06e4
qiine <qm@asuround.local>
2026-04-29 22:05:29 +0200

    feat: dim inactive pane off

--- rendering.lua
+++ rendering.lua
@@ -13,10 +13,16 @@ function M.apply_to_config(config)
     config.front_end = "OpenGL" -- OpenGL, WebGpu, Software
     config.prefer_egl = true
 
+
     config.animation_fps = 100
+    config.max_fps = 120
 
-    config.max_fps = 100
+    -- config.show_fps = true
 
+    config.inactive_pane_hsb = {
+        saturation = 1.0,
+        brightness = 1.0,
+    }
 end
 
 return M
